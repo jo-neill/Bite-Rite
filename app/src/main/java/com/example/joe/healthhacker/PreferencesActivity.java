dfc3fe16e21428746efbbe29446d60053fcaede1
jaskkro <jaskkro@gmail.com>
2017-04-09 11:21:27 -0400

    Merge branch 'master' of https://github.com/jo-neill/HealthHacker
    # Conflicts:
    #	app/src/main/java/com/example/joe/healthhacker/PreferencesActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/joe/healthhacker/PreferencesActivity.java
+++ app/src/main/java/com/example/joe/healthhacker/PreferencesActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -22,6 +23,12 @@ public class PreferencesActivity extends AppCompatActivity {
         private List<Item> itemList = new ArrayList<Item>();
         private Score editScore;
 
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+        Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
+        Spinner spinner5 = (Spinner) findViewById(R.id.spinner5);
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -35,22 +42,35 @@ public class PreferencesActivity extends AppCompatActivity {
                 }
             });
 
-            Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
             ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.choices, android.R.layout.simple_spinner_item);
-            Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
             ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.choices, android.R.layout.simple_spinner_item);
-            Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
             ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.choices, android.R.layout.simple_spinner_item);
-            Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
             ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this, R.array.choices, android.R.layout.simple_spinner_item);
-            Spinner spinner5 = (Spinner) findViewById(R.id.spinner5);
             ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this, R.array.choices, android.R.layout.simple_spinner_item);
         }
 
         private void saveChanges() {
             ArrayList<Restriction> editRestrictions = editScore.getRestrictions();
 
+            //Card 1
+            editRestrictions.get(0).setDesirability(spinner1.getSelectedItem().toString());
+            editRestrictions.get(0).changeStatus(((CheckBox) findViewById(R.id.checkBox1)).isChecked());
 
+            //Card 2
+            editRestrictions.get(1).setDesirability(spinner2.getSelectedItem().toString());
+            editRestrictions.get(1).changeStatus(((CheckBox) findViewById(R.id.checkBox2)).isChecked());
+
+            //Card 3
+            editRestrictions.get(2).setDesirability(spinner3.getSelectedItem().toString());
+            editRestrictions.get(2).changeStatus(((CheckBox) findViewById(R.id.checkBox3)).isChecked());
+
+            //Card 4
+            editRestrictions.get(3).setDesirability(spinner4.getSelectedItem().toString());
+            editRestrictions.get(3).changeStatus(((CheckBox) findViewById(R.id.checkBox4)).isChecked());
+
+            //Card 5
+            editRestrictions.get(4).setDesirability(spinner5.getSelectedItem().toString());
+            editRestrictions.get(4).changeStatus(((CheckBox) findViewById(R.id.checkBox5)).isChecked());
         }
 
 
