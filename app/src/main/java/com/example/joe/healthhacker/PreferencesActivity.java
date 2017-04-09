ffe22a031c45e8dd0e365d2c74db26f2efeb5bb6
jo-neill <jjo5151@psu.edu>
2017-04-09 10:52:04 -0400

    Save button

--- app/src/main/java/com/example/joe/healthhacker/PreferencesActivity.java
+++ app/src/main/java/com/example/joe/healthhacker/PreferencesActivity.java
@@ -15,18 +15,25 @@ import android.content.ClipData.Item;
 import java.util.ArrayList;
 import java.util.List;
 import android.widget.ListAdapter;
+import android.widget.Spinner;
 public class PreferencesActivity extends AppCompatActivity {
 
-       private ListView listView;
-        private ListAdapter listAdapter;
         private List<Item> itemList = new ArrayList<Item>();
-
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_preferences);
 
-
+            Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+            ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.choices, android.R.layout.simple_spinner_item);
+            Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+            ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.choices, android.R.layout.simple_spinner_item);
+            Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+            ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.choices, android.R.layout.simple_spinner_item);
+            Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
+            ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this, R.array.choices, android.R.layout.simple_spinner_item);
+            Spinner spinner5 = (Spinner) findViewById(R.id.spinner5);
+            ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this, R.array.choices, android.R.layout.simple_spinner_item);
         }
 
 
