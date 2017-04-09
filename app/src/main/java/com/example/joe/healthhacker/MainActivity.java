585111044889433756f2584990d9f9b7c5ac0f65
jo-neill <jjo5151@psu.edu>
2017-04-09 06:57:26 -0400

    First working product.

--- app/src/main/java/com/example/joe/healthhacker/MainActivity.java
+++ app/src/main/java/com/example/joe/healthhacker/MainActivity.java
@@ -1,28 +1,29 @@
 package com.example.joe.healthhacker;
-
 import android.os.Bundle;
+import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-
+import android.content.Intent;
+import com.example.joe.healthhacker.Utility.Product;
 import com.example.joe.healthhacker.Service.NutritionService;
 import com.example.joe.healthhacker.Service.NutritionServiceCallBack;
-import com.example.joe.healthhacker.Utility.Product;
+import android.widget.EditText;
 import com.example.joe.healthhacker.Utility.Score;
-
 public class MainActivity extends AppCompatActivity implements NutritionServiceCallBack{
 
     private NutritionService service;
     private Product product;
-    
+    static MainActivity activityA;
+
     private Score appScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activityA = this;
 
         final Button button = (Button) findViewById(R.id.buttonSearchUPC);
         button.setOnClickListener(new View.OnClickListener() {
@@ -33,12 +34,16 @@ public class MainActivity extends AppCompatActivity implements NutritionServiceC
 
     }
 
+    public static MainActivity getInstance(){
+        return activityA;
+    }
+
     public void runService(){
-        appScore = new Score();
         EditText et=(EditText)findViewById(R.id.upc_text);
         String upc = et.getText().toString();
         service = new NutritionService(this);
         service.refreshNutrition(upc);
+        appScore = new Score();
     }
 
     public void serviceSuccess(String[] itemInfo) {
@@ -50,10 +55,22 @@ public class MainActivity extends AppCompatActivity implements NutritionServiceC
         product = new Product(itemName, itemDesc, ingredients);
         Log.d(itemName, itemDesc);
         appScore.refreshScore(product);
+
+        launchActivity();
     }
 
     public Product getProduct(){
         return product;
+    }
+
+    public Score getScore(){
+        return appScore;
+    }
+
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, ResultsActivity.class);
+        startActivity(intent);
     }
 
     public void serviceFailure(Exception exception) {
--- app/src/main/java/com/example/joe/healthhacker/ResultsActivity.java
+++ app/src/main/java/com/example/joe/healthhacker/ResultsActivity.java
@@ -2,12 +2,38 @@ package com.example.joe.healthhacker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.view.View;
+import android.widget.TextView;
+import com.example.joe.healthhacker.Utility.Score;
+import com.example.joe.healthhacker.Utility.Product;
+import com.example.joe.healthhacker.Utility.Score;
+
 public class ResultsActivity extends AppCompatActivity {
 
-    protected void onCall(Bundle savedInstanceState) {
+    public String nameTxt, scoreTxt, badTxt, fineTxt;
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        Product prod = MainActivity.getInstance().getProduct();
+        Score scoreIn = MainActivity.getInstance().getScore();
+
+        nameTxt = prod.getName();
+        fineTxt = prod.getIngredients();
+        scoreTxt = scoreIn.getScore();
+        badTxt = scoreIn.getBadIngredients();
+
+        final TextView name = (TextView) findViewById(R.id.textName);
+        name.setText(nameTxt);
+
+        final TextView score = (TextView) findViewById(R.id.textScore);
+        score.setText(scoreTxt);
+
+        final TextView bad = (TextView) findViewById(R.id.textBad);
+        bad.setText(badTxt);
+
+        final TextView fine = (TextView) findViewById(R.id.textFine);
+        fine.setText(fineTxt);
+
     }
 }
